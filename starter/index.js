7c5dc6689535f18e711cfae3dc96b3946f43e94b
teeteetian <Teetee_tian@outlook.com>
2024-01-21 21:22:30 +0000

    added timeslots using bootstrap

--- starter/index.js
+++ starter/index.js
@@ -3,3 +3,16 @@
 
 
 // use css clases to correct timeblock
+
+// * Display the current day at the top of the calender when a user opens the planner.
+ 
+// * Present timeblocks for standard business hours when the user scrolls down.
+ 
+// * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+ 
+// * Allow a user to enter an event when they click a timeblock
+
+// * Save the event in local storage when the save button is clicked in that timeblock.
+
+// * Persist events between refreshes of a page
+
