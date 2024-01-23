4faae2e29df64ba024e99fa76a4475f59c402c23
teeteetian <Teetee_tian@outlook.com>
2024-01-23 18:37:13 +0000

    added current and past style

--- starter/index.js
+++ starter/index.js
@@ -1,16 +1,46 @@
 var date = dayjs().format("dddd D MMMM YYYY");
-// var currentHour = dayjs().hour().format("h m");
-// var timeZones = $("textarea");
+var currentHour = dayjs().hour()
+var timeZones = $("textarea");
 
 // var currentHour = dayjs().hour();
+
+//.hour shows current time
+//id for idv hours. hour-9 change
+
 
 // var past = ()
 // var current = ()
 // var future = ()
 
-// set all text area to current
-// $(timeZones).appendTo(".present");
-$("textarea").addClass("present");
+// show current time
+$(`#hour-${currentHour}`).addClass("present");
+
+// future
+//create array called hours
+// 9-5
+//loop through , if current item is less that current hour add certan class 
+// else add this class
+
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+for (let i = 0; i < hours.length; i++) {
+   console.log(hours[i])
+
+var loopValue = hours[i]
+
+if (loopValue < currentHour){
+    $(`#hour-${loopValue}`).addClass("past");
+    console.log(loopValue, "im in the past")
+}
+else {
+    console.log(loopValue, "im not in the past")
+}
+
+}
+
+$(`#hour-${currentHour}`).addClass("present");
+
+
+
 
 
 // * Display the current day at the top of the calender when a user opens the planner.
@@ -21,8 +51,7 @@ $("#currentDay").text(date);
 
 
 
-var nine= $("#nineAm");
-$("nine").css("present");
+
 
 
  
